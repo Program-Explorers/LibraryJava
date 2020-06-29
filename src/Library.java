f4b658f7eb3220a1b4e3da7e7365fdacd268d7b0
FlashCoder101 <anishwestwindsor@gmail.com>
2020-06-29 17:23:26 -0400

    Made the book library with download links for each book

---
+++ src/Library.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Library{
+    public static void main(String [] args){
+        int userBookChoice;
+        String[] list = {"http://www.gutenberg.org/ebooks/1727.epub.images?session_id=826f005e82cf66fc8e8790fdcc44ea9c2c1b8de9"
+        , "https://bit.ly/2xTJMzm"
+        , "https://www.planetebook.com/free-ebooks/1984.pdf"
+        , "https://www.pdfdrive.com/download.pdf?id=3439748&h=099c1f52ca2f8fd7306dad8372d6f535&u=cache&ext=pdf"};
+        
+        Scanner keyboard = new Scanner(System.in);
+
+        
+        System.out.println("Welcome to your book library!\nIn this library, you can enter the number of the\n"
+        + "book you want to read and we will provide the website link for you to download or view the book");
+
+        System.out.print("\n1.The Odyssey"
+        + "\n2. Fellowship of the Ring" + "\n3. 1984" + "\n4. Percy Jackson and the Lightning Thief"
+        + "\nEnter the number of the book you want: ");
+        userBookChoice = keyboard.nextInt();
+
+        System.out.println(list[userBookChoice - 1]);
+
+        
+    }
+}
